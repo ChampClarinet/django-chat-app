d41b631bc74c40cfd9ab9ee603c5261b5a2a88c4
Wallop Opasakhun <champbassist159@gmail.com>
2020-08-29 23:56:13 +0700

    initial

---
+++ backend/chat/consumers.py
@@ -0,0 +1,44 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        #!last array key must same with name var in ws patterns
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        #? Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.room_name
+        )
+
+    async def receive(self, text_data):
+        print('passed receive')
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    #? Receive messages from client
+    async def chat_message(self, event):
+        print('passed chat_message')
+        message = event['message']
+
+        await self.send(text_data = json.dumps({
+            'message': message
+        }))
